6001039af5702cb7f62ad33fd559fd32dcf5d2e6
Amin9527 <861231792@qq.com>
2018-10-06 23:24:07 -0700

    c++ question

---
+++ c++/Question/question.cpp
@@ -0,0 +1,20 @@
+//cout<<"佛祖保佑"<<endl<<"张敏，加油！"<<endl;
+
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string zm1="张敏";
+	string str="1234";
+	string zm="zhangmin";
+	cout<<str.len()<<endl;
+	cout<<sizeof(str)<<endl;
+	cout<<sizeof(zm)<<endl;
+	return 0;
+}
+
+
+
+
