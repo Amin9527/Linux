3e969b8b1104beb1211caac8beb55455f63daeb6
Amin9527 <861231792@qq.com>
2018-10-29 04:09:30 -0700

    2018/10/29

--- c++/Question/question.cpp
+++ c++/Question/question.cpp
@@ -2,8 +2,40 @@
 
 #include<string>
 #include<iostream>
+#include<stdio.h>
 using namespace std;
+int main()
+{
+    int m=0x1a00;
+    int n=0x001a;
+    cout<<m<<endl<<n<<endl;
+    union un
+    {
+        int l;
+        char ch;
+    }un1;
+    un1.l=0x1a00002a;
+    printf("%d\n",un1.ch);
+    return 0;
+}
 
+///////////////////////////////////////////////////////////////
+
+//  %  与   /
+/*
+int main()
+{
+    cout<<2%10<<endl;
+    cout<<2/10<<endl;
+    cout<<16/10<<endl;
+    return 0;
+}
+*/
+
+//////////////////////////////////////////////////////////////////
+
+//string类大小的计算
+/*
 int main()
 {
 	string zm1="张敏高兴z";
@@ -14,6 +46,7 @@ int main()
 	cout<<zm.size()<<endl;
 	return 0;
 }
+*/
 
 
 
